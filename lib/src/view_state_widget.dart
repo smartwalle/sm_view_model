2e80982ceb2265d89574ea5f35514890e8c022da
smartwalle <smartwalle@gmail.com>
2022-05-23 15:17:56 +0800

    add ViewStateWidget

--- lib/src/view_state_widget.dart
+++ lib/src/view_state_widget.dart
@@ -3,9 +3,18 @@ import 'package:sm_view_state/src/view_state.dart';
 
 class ViewStateWidget<T> extends StatelessWidget {
   final ViewState<T> state;
+
+  /// 当 [ViewState] 的 [state] 属性为 [LoadState.finished] 时，会构建本方法返回的 [Widget]
+  /// 当 [ViewState] 的 [state] 属性不为 [LoadState.finished]，但是没有提供相应状态的 [Widget]，也会构建本方法返回的 [Widget]
   final Widget Function(BuildContext context, ViewState<T> state) builder;
+
+  /// 当 [ViewState] 的 [state] 属性为 [LoadState.init] 时，会构建本方法返回的 [Widget]
   final Widget Function(BuildContext context, ViewState<T> state)? init;
+
+  /// 当 [ViewState] 的 [state] 属性为 [LoadState.loading] 时，会构建本方法返回的 [Widget]
   final Widget Function(BuildContext context, ViewState<T> state)? loading;
+
+  /// 当 [ViewState] 的 [error] 属性不为 [null] 时，会优先构建本方法返回的 [Widget]
   final Widget Function(BuildContext context, Object error)? error;
 
   const ViewStateWidget({
@@ -19,14 +28,14 @@ class ViewStateWidget<T> extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    if (state.hasError && error != null) {
+      return error!(context, state.error!);
+    }
     if (state.isInit && init != null) {
       return init!(context, state);
     }
     if (state.isLoading && loading != null) {
       return loading!(context, state);
-    }
-    if (state.hasError && error != null) {
-      return error!(context, state.error!);
     }
     return builder(context, state);
   }
